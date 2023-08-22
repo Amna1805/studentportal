7813d9853994fbe0b52f7f97ace7af06abdcfd18
laibamasod <laibamasoodd@gmail.com>
2023-08-22 12:39:41 +0500

    validations under progress

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
+//import VeeValidatePlugin from '@vee-validate';
+//import { defineRule } from '@vee-validate/rules';
 
+
+// defineRule('cnic', (value) => {
+//     return /^[0-9]{13}$/.test(value);
+//   });
 const appInstance = createApp(App);
+//appInstance.use(VeeValidatePlugin);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
